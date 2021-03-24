b83477286f9685730ac75a84b671fb1cac2235bf
Paulo Faria <paulo.faria.rl@gmail.com>
2021-03-24 19:08:21 -0300

    refactor: Improvements to HexadecimalData and binary data related APIs

--- Sources/Tendermint/Libs/Bytes/Bytes.swift
+++ Sources/Tendermint/Libs/Bytes/Bytes.swift
@@ -1,15 +1,49 @@
 import Foundation
 
 // The main purpose of HexBytes is to enable HEX-encoding for json/encoding.
-public struct HexadecimalData: RawRepresentable, Codable {
+public struct HexadecimalData: DataProtocol, RawRepresentable, Codable, CustomStringConvertible {
+    public typealias Regions = Data.Regions
+    public typealias Element = Data.Element
+    public typealias Index = Data.Index
+    public typealias SubSequence = Data.SubSequence
+    public typealias Indices = Data.Indices
+    
+    public var regions: Data.Regions {
+        rawValue.regions
+    }
+    
+    public var startIndex: Data.Index {
+        rawValue.startIndex
+    }
+    
+    public var endIndex: Data.Index {
+        rawValue.endIndex
+    }
+    
+    public subscript(position: Data.Index) -> Data.Element {
+        rawValue[position]
+    }
+
     public var rawValue: Data
     
     public init?(rawValue: Data) {
         self.rawValue = rawValue
     }
     
-    public init(_ data: Data) {
-        self.rawValue = data
+    public init<D: DataProtocol>(_ data: D) {
+        self.rawValue = Data(data)
+    }
+    
+    public init<S: Sequence>(_ sequence: S) where S.Element == UInt8 {
+        self.rawValue = Data(sequence)
+    }
+    
+    public init?<S: StringProtocol>(_ string: S) {
+        guard let data = Data(hexEncoded: string) else {
+            return nil
+        }
+        
+        self.init(data)
     }
     
     public init(from decoder: Decoder) throws {
@@ -33,5 +67,9 @@ public struct HexadecimalData: RawRepresentable, Codable {
     
     public var isEmpty: Bool {
         rawValue.isEmpty
+    }
+    
+    public var description: String {
+        rawValue.hexEncodedString(options: .upperCase)
     }
 }
