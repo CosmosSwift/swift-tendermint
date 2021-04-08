fddda2da6058940f95f3e8b56eba8fe10488152e
Alex Tran Qui <alex@katalysis.io>
2021-04-08 09:42:49 +0200

    -Implement ErrorWrapper

--- Sources/Tendermint/RESTClient/ErrorWrapper.swift
+++ Sources/Tendermint/RESTClient/ErrorWrapper.swift
@@ -1,3 +1,18 @@
 public struct ErrorWrapper: Error, Codable {
-    #warning("TODO: implement ErrorWrapper")
+   public let code: Int
+    public let message: String
+    public let error: String
+
+    public init(code: Int = -1, message: String = "Unknown", error: String = "Can't deserialize error.") {
+        self.code = code
+        self.message = message
+        self.error = error
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.code = (try? container.decode(Int.self, forKey: .code)) ?? -1
+        self.message = (try? container.decode(String.self, forKey: .message)) ?? "Unknown"
+        self.error = (try? container.decode(String.self, forKey: .error)) ?? "Not able to decode"
+    }
 }
